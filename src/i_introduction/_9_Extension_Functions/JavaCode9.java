607e34eb3df695ce332356b1f28d7d367e17055a
NN <nnworkspace@gmail.com>
2018-11-14 09:13:40 +0100

    solved problems 6 - 9

--- src/i_introduction/_9_Extension_Functions/JavaCode9.java
+++ src/i_introduction/_9_Extension_Functions/JavaCode9.java
@@ -3,7 +3,8 @@ package i_introduction._9_Extension_Functions;
 import util.JavaCode;
 
 public class JavaCode9 extends JavaCode {
-    public void useExtension() {
-        char c = N09ExtensionFunctionsKt.lastChar("abc");
-    }
+
+  public void useExtension() {
+    char c = N09ExtensionFunctionsKt.lastChar("abc");
+  }
 }
